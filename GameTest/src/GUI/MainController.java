f47e44b74979846b37d2a233660d374364de1480
Tiran1990 <t.hesawi1990@gmail.com>
2025-03-22 20:48:17 +0200

    hello

---
+++ GameTest/src/GUI/MainController.java
@@ -0,0 +1,60 @@
+package GUI;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainController {
+	 @Override
+	    public void start(Stage primaryStage) throws Exception {
+	        // יצירת חיבור למסד נתונים
+	       /* databaseConnection = new DatabaseConnection();
+	        conn = databaseConnection.getconnection();
+
+	        if (conn == null) {
+	            throw new IllegalStateException("Cannot connect to the database");
+	        }*/
+	        FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
+	        Scene scene1 = new Scene(loader.load());
+	     
+	     // Set the first screen as the initial scene
+	        primaryStage.setTitle("Scene Switcher Example");
+	        primaryStage.setScene(scene1);
+	        primaryStage.show();
+	       // AppController aFrame = loader.getController();// create StudentFrame
+	      //  aFrame.setStage(primaryStage);
+
+			/*
+	        // טעינת ממשק המשתמש הראשי
+	        URL fxmlLocation = getClass().getResource("/com/screen/main.fxml");  // עדכון נתיב ה-FXML
+	        if (fxmlLocation == null) {
+	            throw new IllegalStateException("FXML file not found.");
+	        }
+
+	        FXMLLoader loader = new FXMLLoader(fxmlLocation);
+	        Parent root = loader.load();
+
+	        // העברת החיבור למסד הנתונים למחלקת הבקרה
+	        AppController controller = loader.getController();
+	        controller.getClass();
+
+	        primaryStage.setTitle("Wine Database Application");
+	        primaryStage.setScene(new Scene(root));
+	        primaryStage.show();
+	*/
+	        // סגירת החיבור למסד הנתונים ביציאה
+	    /*    primaryStage.setOnCloseRequest(event -> {
+	            try {
+	                if (conn != null && !conn.isClosed()) {
+	                    conn.close();
+	                    System.out.println("Database connection closed.");
+	                }
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	            }
+	        });*/
+	    }
+
+	    public static void main(String[] args) {
+	        launch(args);
+	    }
+}
---
+++ GameTest/src/Main/GameMain.java
@@ -0,0 +1,51 @@
+package Main;
+
+import java.sql.Connection;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GameMain extends Application{
+    private Connection conn;
+   // private DatabaseConnection databaseConnection;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // יצירת חיבור למסד נתונים
+       /*databaseConnection = new DatabaseConnection();
+       conn = databaseConnection.getconnection();
+
+        if (conn == null) {
+            throw new IllegalStateException("Cannot connect to the database");
+        }
+        */
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Scene scene1 = new Scene(loader.load());
+     
+     // Set the first screen as the initial scene
+        primaryStage.setTitle("Scene Switcher Example");
+        primaryStage.setScene(scene1);
+        primaryStage.show();
+       // AppController aFrame = loader.getController();// create StudentFrame
+        //aFrame.setStage(primaryStage);
+
+
+        // סגירת החיבור למסד הנתונים ביציאה
+        primaryStage.setOnCloseRequest(event -> {
+            try {
+                if (conn != null && !conn.isClosed()) {
+                    conn.close();
+                    System.out.println("Database connection closed.");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
